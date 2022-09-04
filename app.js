d1fbeb0ddba6700ea40aa6d213a0dbb258d5dff4
Janak Kapadia <kapadia.janak@gmail.com>
2022-09-04 18:33:26 +0530

    Add about and 404 page and install express js

--- app.js
+++ app.js
@@ -1,23 +1,37 @@
 const http = require("http");
-let PORT = 3000;
-const URL = 'http://localhost'
+const fs = require("fs");
 
-let args = process.argv
-let port = args.filter(a => a.startsWith('port='))
+let PORT = 3000;
+const URL = 'http://localhost';
+
+let args = process.argv;
+let port = args.filter(a => a.startsWith('port='));
 if (port.length) {
-  PORT = port[0].split('=')[1]
+  PORT = port[0].split('=')[1];
 }
 
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('<h1>Hello World</h1>');
+    fs.readFile("Pages/home.html",'utf-8',(err, data) => {
+      if (err) throw err;
+      res.write(data);
+      res.end();
+    })
   }else if (req.url === '/about') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('<h1>I am about page</h1>');
+    fs.readFile("Pages/about.html",'utf-8',(err, data) => {
+      if (err) throw err;
+      res.write(data);
+      res.end();
+    })
   }else{
     res.writeHead(404, { 'Content-Type': 'text/html' });
-    res.end('<h1>Page Not Found</h1>');
+    fs.readFile("Pages/404.html",'utf-8',(err, data) => {
+      if (err) throw err;
+      res.write(data);
+      res.end();
+    })
   }
 })
 console.log(`Server is running at ${URL}:${PORT}`)
